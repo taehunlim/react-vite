c1837f44c50ac2a1fe4ef4a6239604297b2bf6ec
taehunlim <dsa1520@naver.com>
2022-09-25 16:44:27 +0900

    Feat: eslint, prettier, absolute path

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-import { App } from "./routes";
-import EmotionProvider from "./assets/EmotionProvider";
+import { App } from './routes';
+import EmotionProvider from './assets/EmotionProvider';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <EmotionProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </EmotionProvider>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+   <EmotionProvider>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </EmotionProvider>,
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from 'react';
+import styled from '@emotion/styled';
 
 function Home() {
-  return (
-    <div>
-      <Span>react</Span>
-    </div>
-  );
+   return (
+      <div>
+         <Span>react</Span>
+      </div>
+   );
 }
 
 const Span = styled.span`
-  color: ${({ theme }) => theme.fg.danger};
+   color: ${({ theme }) => theme.fg.danger};
 `;
 
 export default Home;
