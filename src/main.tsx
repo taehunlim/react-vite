b29b9dcf9bb0354495738796971f0d5b874f1441
taehunlim <dsa1520@naver.com>
2022-10-24 16:56:53 +0900

    Feat: ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,18 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './routes';
 import EmotionProvider from './assets/EmotionProvider';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
    <EmotionProvider>
       <BrowserRouter>
-         <App />
+         <ErrorBoundary FallbackComponent={<Error />}>
+            <App />
+         </ErrorBoundary>
       </BrowserRouter>
    </EmotionProvider>,
 );
+
+function Error() {
+   return <h1 style={{ color: 'black' }}>Application Error</h1>;
+}
