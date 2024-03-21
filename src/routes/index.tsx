c77335b432d0aa9a939abe5fc3f8dfed2dc58261
taehunlim <dsa1520@naver.com>
2024-03-21 23:12:56 +0900

    Clean: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,8 @@
-import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { generateRoutes } from './core';
 
-type ElementType = {
-   [key: string]: () => JSX.Element;
-};
+const ROUTES = import.meta.glob('/src/pages/**/[a-z[]*.tsx', { eager: true });
 
-const ROUTES: Record<string, ElementType> = import.meta.glob(
-   '/src/pages/**/[a-z[]*.tsx',
-   { eager: true },
-);
+export const routes = generateRoutes(ROUTES);
 
-export const routes: RouteObject[] = generateRoutes(ROUTES);
-
-console.log(routes);
 export const router = createBrowserRouter(routes);
